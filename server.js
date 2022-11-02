d14d77d1203022745223db85688bd22cd48ad5ce
= <=>
2022-11-01 20:59:16 -0500

    Changed Websocket Code and added code for uploading and error404

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 Dai Dai Vo - 3129620
 Robinpreet Singh - 3127986
 Brody Oberdorfer - 3135170 */
-
+const multiparty = require('multiparty')
 const express = require('express');
 const fs = require("fs");
 const path = require("path")
@@ -12,6 +12,10 @@ const expressWs = require("express-ws")
 
 const app = express();
 const port = 5000;
+
+
+app.use('/', express.static(__dirname))
+app.use(express.urlencoded({extended:true}))
 
 //put our mongodb uri here
 const mongoUri = "mongodb+srv://dai:09022002@cluster0.esqge8e.mongodb.net/?retryWrites=true&w=majority";
@@ -29,16 +33,21 @@ async function check_add_name(username){
     console.log(doc)
 }
 
-async function check_add_chatroomName(chatroomName){
+async function check_add_chatroomName(chatroomName, req, res){
     await client.connect();
     const myCol = await client.db('express').collection("chatroomName");
-    const doc = await myCol.findOne({chatroom:chatroomName})
+    let doc = await myCol.findOne({chatroom:chatroomName})
     if(doc === null){
         let newChatroom = {chatroom:chatroomName}
         let result = await myCol.insertOne(newChatroom)
-        console.log(result)
+        console.log("RoomName",result)
+        req.body.message = true;
+        res.status(200).send(req.body)
+    }else{
+        console.log("Chatroom already exsists")
+        req.body.message = "exsists"
+        res.send(req.body)
     }
-    console.log(doc)
 }
 
 const wsInstance = expressWs(app)
@@ -57,37 +66,90 @@ app.get("/", (req, res)=>{
 })
 
 // this will go to chatbox.njk. Which will create websocket chatroom if not exist, or join that chatroom if exist
-app.get("/chatroom/:name", (req, res)=>{
+
+app.get("/chatroom/:name", async (req, res)=>{
     let {name} = req.params;
-    console.log(name)
-    res.status(200).render("chatbox.njk", {title: `${name}`})
+    console.log("Name of Room",name)
+    await client.connect();
+    const myCol = await client.db('express').collection("chatroomName");
+    let doc = await myCol.findOne({chatroom:name})
+    if(doc === null){
+        // we will send custom 404 here
+        res.status(404).send("The page does not exsists")
+    }else{
+        res.status(200).render("chatbox.njk", {title: `${name}`})
+    }
+    
 })
 
 // handle the post request of createRoom, and send data back to the server side
 app.post("/createRoom", async (req, res)=>{
-    
-    req.body.message = "true";
     let user = req.body.user;
     let roomName = req.body.roomName;
-    check_add_name(user)
-    check_add_chatroomName(roomName)
-    res.status(200).send(req.body)
+    await check_add_chatroomName(roomName, req, res);
 })
 
 app.post("/formhandler2", (req, res)=>{
 
 })
 
-app.ws("/chatroom/1", async (ws, req)=>{
-    //get websocket server
-    const aWss = wsInstance.getWss("/chatroom/1");
-    // when websocket server receive data
-    ws.on("message", (msg)=>{
-        // send data back to every client
-        aWss.clients.forEach((client)=>{
-            client.send(msg)
+
+app.post('/roomName', (req, res)=>{
+    let room = Object.keys(req.body)[0];
+    app.ws(`/chatroom/${room}`, async (ws, req)=>{
+        //get websocket server
+        const aWss = wsInstance.getWss(`/chatroom/${room}`);
+        // when websocket server receive data
+        ws.on("message", (msg)=>{
+            // send data back to every client
+            aWss.clients.forEach((client)=>{
+                client.send(msg)
+            })
         })
     })
+    
+})
+
+
+let mimeLookup = {
+    ".html" : "text/html",
+    ".jpg": "image/jpeg",
+    ".js": "application/javascript",
+    ".png" : "image/png"
+}
+
+app.post("/chatroom/fileUpload",(req, res)=>{
+    let response = "";
+    let form = new multiparty.Form({autoFields:true})
+    form.on('part', (part)=>{
+        
+        let partName = part.filename
+        let part_ext = path.extname(partName)
+        let partType = mimeLookup[part_ext]
+        if(!partType){
+            part.resume();
+            response = "This file type is not supported"
+            return
+        }
+        if (part.name !== "filename"){
+            part.resume();
+            return
+        }
+        // if(part.byteCount > 3000000){
+        //     part.resume();
+        //     response = "This file is too large"
+        //     return
+        // }
+
+        part.pipe(fs.createWriteStream(path.join('./public' , part.filename)))
+
+        
+        response = `file with name ${part.filename} saved`
+    })
+    form.on("close", ()=>{
+        res.send(response)
+    })
+    form.parse(req)
 })
 
 app.listen(port)
